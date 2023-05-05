3af79c12b7b5f20cd7816300dc739c975eb7b9dd
kgorna <karolinagorna@hotmail.fr>
2023-05-05 18:06:26 +0200

    add ex3

---
+++ ex3/template.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+
+import "token.sol";
+
+contract TestToken is Token {
+    address echidna_caller = msg.sender;
+
+    constructor() public {
+        balances[echidna_caller] = 10000;
+    }
+
+    // add the property
+    function echidna_test_balance() public view returns (bool) {}
+}
